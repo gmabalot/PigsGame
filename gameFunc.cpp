0a89ab3980f13eb4138381f8e852265109374210
Michael Spencer <MSpencer87@gmail.com>
2015-09-15 18:30:41 -0700

    Added Function Descriptions

--- gameFunc.cpp
+++ gameFunc.cpp
@@ -10,12 +10,16 @@
 #include <iostream>
 
 #include "gameFunc.h"
-#include "player.h"
-#include "die.h"
+#include "Player.h"
+#include "Dice.h"
 
 using std::string;
 using std::vector;
 
+/*
+ * startMessage - this function displays the game title and has the player
+ * set their name and game attributes
+*/
 void GameFunc::startMessage(){
 	string gameName[8] = {" _____ _            _____                     ",
  	"|  __ (_)          / ____|                     ",
@@ -45,8 +49,9 @@ void GameFunc::startMessage(){
 		additionalOptions();
 }
 
-
-
+/*
+ * beginGame - this function begins the game
+ */
 void GameFunc::beginGame(){
 	//Select random player to start.
 
@@ -56,13 +61,29 @@ void GameFunc::beginGame(){
 
 }
 
+
+/*
+ * gameEnd - this function ends the game
+ */
 void GameFunc::gameEnd(){
 }
 
+
+/*
+ * getNumPlayers - this function returns the number of players
+ * @return - int - the number of players
+ */
 int GameFunc::getNumPlayers(){
 	return m_numPlayers;
 }
 
+
+/*
+ * getPlayerChoice - this function allows the player to continue rolling
+ * or to hold at their current roll
+ * @param - Player p - the player making the choice
+ * @return - bool - 
+ */
 bool GameFunc::getPlayerChoice(Player p){
 	int i = p.getChoice(); //return 0 or 1 for hold/roll
 	if(i == 1)
@@ -73,16 +94,30 @@ bool GameFunc::getPlayerChoice(Player p){
 	}
 }
 
+
+/*
+ * setName - this function sets the players name
+ * @return - bool -
+ */
 bool GameFunc::setName(string name, Player p){
 	//Call player.setName and handle errors
 	p.setName(name);
 }
 
+/*
+ * resetScore - this function resets the score
+ * @return - bool -
+ */
 bool GameFunc::resetScore(){
 	for(Player curPlayer : pVec)
 		curPlayer.score = 0;
 }
 
+/*
+ * changeDiffiiculty - this function changes the AI difficulty
+ * @param - int i - integer to describe difficulty level
+ * @return - bool -
+ */
 bool GameFunc::changeDifficulty(int i){
 	//Potentially move this const;
 	for( Player curPlayer : pVec)
@@ -91,6 +126,11 @@ bool GameFunc::changeDifficulty(int i){
 	}
 }
 
+/*
+ * rollDie - this function rolls the dice for the player
+ * @param - Die - the die that is being rolled
+ * @return - int
+ */
 int GameFunc::rollDie(Die d){
 	int dieNum = d.roll;
 	for(int i : m_passNums)
@@ -101,31 +141,59 @@ int GameFunc::rollDie(Die d){
 		if(!changeTurn)
 			//console.log("Error changing turn.");
 
-
+/*
+ * updateHoldScore - this function updates the score being held and
+ * checks to see if the plaer has won
+ * @param - Player - the current player whose score is being updated 
+ */
 void GameFunc::updateHoldScore(Player p){
 	p.score += holdPoints;
 	holdPoints = 0;
 	checkWinner(p);
 }
 
+/*
+ *
+ *
+ *
+ *
+ */
 bool GameFunc::changeTurn(){
 	
 }
 
+/*
+ * checkWinner - checks if score is high enough to win
+ *
+ */
 bool GameFunc::checkWinner(){
 	if (p.getPoints >= m_endScore)
 		gameEnd();
 }
 
+/*
+ *
+ *
+ *
+ */
 void GameFunc::addNumToPassNums(vector<int> nums){
 	for(int i : nums)
 		m_passNums.push(i);
 }
 
+/*
+ * setEndScore - sets the toatal amount of points needed to win
+ * @param - int - the goal/winning number to use
+ */
 void GameFunc::setEndScore(int i){
 	m_endScore = i;
 }
 
+/*
+ * addPlayer - adds an individual player to the game
+ * @param - string - the name of the player
+ * @param - int - if the player is human or AI
+ */
 void GameFunc::addPlayer(string playerName, int playerType){
 	if(playerType == 0)
 		Player p = new Player(player);
@@ -136,6 +204,9 @@ void GameFunc::addPlayer(string playerName, int playerType){
 
 
 
+/*
+ * addPlayers - adds players to the game
+ */
 void GameFunc::addPlayers(){
 
 }
